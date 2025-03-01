1c9ff40da19b28e2439c8fcc68975b2b543ca280
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-03-01 13:49:11 +0000

    fixes

--- app/components/TypingAnimation.tsx
+++ app/components/TypingAnimation.tsx
@@ -1,5 +1,5 @@
 import { TypingAnimation } from "../../components/magicui/typing-animation";
 
 export function TypingAnimation1() {
-  return <TypingAnimation className="text-white text-center text-4xl font-semibold"> Introducing Fluid Compute</TypingAnimation>;
+  return <TypingAnimation duration={300} delay={0} className="text-white text-center text-4xl font-semibold"> Introducing Fluid Compute</TypingAnimation>;
 }
