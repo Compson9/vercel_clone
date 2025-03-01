c042e57d2810533006af25c82b52fc66be63e287
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-03-01 18:05:58 +0000

    final push

--- app/components/Deploy.tsx
+++ app/components/Deploy.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import ImageMarquee from "./ImageMarquee";
-
 
 export default function Deploy() {
   return (
-    <div className="m-16 p-20 border-gray-600 gap-10 w-[1079px] h-[878px] flex justify-between items-center border-[0.01px]">
+    <div className="m-16 p-20 border-gray-600 gap-10  w-[1079px] h-[878px] flex justify-between items-center border-[0.01px]">
       <div className="first w-[358px] h-[709px] mt-[250px]">
         <h1 className="text-white mb-10 text-3xl">
           Deploy your first app in seconds
@@ -14,12 +12,55 @@ export default function Deploy() {
           width={262}
           height={262}
           alt="CLI"
-          className="cursor-pointer"
+          className="cursor-pointer hover:opacity-60"
         />
       </div>
-
-      {/* Image Marquee Component */}
-      <ImageMarquee />
+      <div className="second flex flex-col gap-5">
+        <Image
+          src="/next.png"
+          width={330}
+          height={170}
+          alt="CLI"
+         className="cursor-pointer hover:opacity-60"
+        />
+        <Image
+          src="/react.png"
+          width={330}
+          height={170}
+          alt="CLI"
+         className="cursor-pointer hover:opacity-60"
+        />
+        <Image
+          src="/astro.png"
+          width={330}
+          height={170}
+          alt="CLI"
+       className="cursor-pointer hover:opacity-60"
+        />
+      </div>
+      <div className="third flex flex-col gap-5">
+        <Image
+          src="/svelte.png"
+          width={330}
+          height={170}
+          alt="CLI"
+        className="cursor-pointer hover:opacity-60"
+        />
+        <Image
+          src="/nuxt.png"
+          width={330}
+          height={170}
+          alt="CLI"
+           className="cursor-pointer hover:opacity-60"
+        />
+        <Image
+          src="/python.png"
+          width={330}
+          height={170}
+          alt="CLI"
+           className="cursor-pointer hover:opacity-60"
+        />
+      </div>
     </div>
   );
 }
